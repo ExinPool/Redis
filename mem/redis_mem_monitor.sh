02b68e97d3369ee4b644851a7f17a7952db772b7
Robin Wen <blockxyz@gmail.com>
2021-12-15 10:53:18 +0800

    Add Redis mem

--- mem/redis_mem_monitor.sh
+++ mem/redis_mem_monitor.sh
@@ -15,21 +15,22 @@ source config.shlib
 # load configuration
 service="$(config_get SERVICE)"
 mem_num="$(config_get MEM_NUM)"
-redis_cli="$(config_get REDIS_CLI)"
 host="$(config_get HOST)"
+port="$(config_get PORT)"
+passwd="$(config_get PASSWD)"
 log_file="$(config_get LOG_FILE)"
 lark_webhook_url="$(config_get LARK_WEBHOOK_URL)"
 sleep_num="$(config_get SLEEP_NUM)"
 
-mem_num_var_1=`echo "info memory" | ${redis_cli} | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
+mem_num_var_1=`echo "info memory" | redis-cli -h $host --tls -a '$passwd' -p $port | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
 sleep ${sleep_num}
-mem_num_var_2=`echo "info memory" | ${redis_cli} | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
+mem_num_var_2=`echo "info memory" | redis-cli -h $host --tls -a '$passwd' -p $port | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
 sleep ${sleep_num}
-mem_num_var_3=`echo "info memory" | ${redis_cli} | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
+mem_num_var_3=`echo "info memory" | redis-cli -h $host --tls -a '$passwd' -p $port | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
 sleep ${sleep_num}
-mem_num_var_4=`echo "info memory" | ${redis_cli} | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
+mem_num_var_4=`echo "info memory" | redis-cli -h $host --tls -a '$passwd' -p $port | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
 sleep ${sleep_num}
-mem_num_var_5=`echo "info memory" | ${redis_cli} | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
+mem_num_var_5=`echo "info memory" | redis-cli -h $host --tls -a '$passwd' -p $port | grep used_memory_human | awk -F':' '{print $2}' | sed "s/G//g"`
 
 sum=`echo "scale=1; ${mem_num_var_1} + ${mem_num_var_2} + ${mem_num_var_3} + ${mem_num_var_4} + ${mem_num_var_5}" | bc -l`
 avg=`echo $sum / 5 | bc -l`
